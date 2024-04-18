96a6c1d3c84e6fc397f0b411618eff82eaf3b27e
Loïc M <loic.mahadzere.pro@gmail.com>
2024-04-18 12:25:04 +0200

    push

--- Accueil.php
+++ Accueil.php
@@ -4,8 +4,19 @@
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="style.css">
             <title>TEST</title>
         </head>
+        <div class="header">
+            <h1>Bienvenue</h1>
+        </div>
+
+        <div class="description">
+            <p>bienvenue sur le site VoteMymusic ici vous pourrez ajouter les musique que vous voudriez entendre lors d'un concert karaoké ou activé en tous genre une
+                 apres avoir ajouter une musique vous pourrez votez entre chaque musique proposer par les utilisateur pour ensuite les musique recoltant le plus de votes seront les 
+                 musique qui seront jouer.
+            </p>
+        </div>
         <body>
 
 
