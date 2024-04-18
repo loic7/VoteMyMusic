abe479ab8e58b3ede60f56742cb97f96dfffffc5
Loïc M <loic.mahadzere.pro@gmail.com>
2024-04-18 14:16:15 +0200

    FINITION ACCUEIL

--- Accueil.php
+++ Accueil.php
@@ -1,28 +1,41 @@
-<php>
-    <!DOCTYPE html>
-    <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="style.css">
-            <title>TEST</title>
-        </head>
-        <div class="header">
-            <h1>Bienvenue</h1>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <nav>
+        <div class="logo">
+            <a href="index.html">VoteMyMusic</a>
         </div>
-
-        <div class="description">
-            <p>bienvenue sur le site VoteMymusic ici vous pourrez ajouter les musique que vous voudriez entendre lors d'un concert karaoké ou activé en tous genre une
-                 apres avoir ajouter une musique vous pourrez votez entre chaque musique proposer par les utilisateur pour ensuite les musique recoltant le plus de votes seront les 
-                 musique qui seront jouer.
-            </p>
+        <ul class="nav-links">
+            <li><a href="index.html">Accueil</a></li>
+            <li><a href="#">À propos</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        <a href="connexion.html" class="btn">Connexion</a>
+    </nav>
+    <header>
+        <h1>Bienvenue</h1>
+    </header>
+    <main class="main">
+        <div class="message-box">
+            <p>Bienvenue sur VoteMyMusic ! Ce site est fier d'être en partenariat avec CVVEN. 
+                Ici, vous avez la possibilité de voter, proposer et soutenir des musiques lors des spectacles organisés sur le site de vacances. 
+                Nous sommes ravis de vous accueillir dans notre communauté passionnée de musique, où votre voix compte et contribue à façonner les expériences musicales inoubliables. 
+                Que la musique vous accompagne dans chaque moment de votre séjour avec nous !</p>
         </div>
-        <body>
-
-
-        </body>
-
-        </html>
-    
-
-    </php>
+        <a href="autre_page.html" class="btn2">Poursuivre</a>
+    </main>
+    <footer>
+        <ul class="footer-links">
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">À propos</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </footer>
+</body>
+</html>
