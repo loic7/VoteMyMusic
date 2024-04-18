c764046345b67bf0ffdfa7734715ec79416dc48a
Loïc M <loic.mahadzere.pro@gmail.com>
2024-04-18 12:39:50 +0200

    test pc maison

--- Accueil.php
+++ Accueil.php
@@ -1,28 +1,22 @@
-<php>
-    <!DOCTYPE html>
-    <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="style.css">
-            <title>TEST</title>
-        </head>
-        <div class="header">
-            <h1>Bienvenue</h1>
-        </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;500&display=swap" rel="stylesheet">
+    <title>TEST</title>
+</head>
+<body>
+    <div class="header">
+        <h1>Bienvenue</h1>
+    </div>
 
-        <div class="description">
-            <p>bienvenue sur le site VoteMymusic ici vous pourrez ajouter les musique que vous voudriez entendre lors d'un concert karaoké ou activé en tous genre une
-                 apres avoir ajouter une musique vous pourrez votez entre chaque musique proposer par les utilisateur pour ensuite les musique recoltant le plus de votes seront les 
-                 musique qui seront jouer.
-            </p>
-        </div>
-        <body>
-
-
-        </body>
-
-        </html>
-    
-
-    </php>
+    <div class="description">
+        <p>bienvenue sur le site VoteMymusic ici vous pourrez ajouter les musiques que vous voudriez entendre lors d'un concert karaoké ou activé en tous genre une
+             après avoir ajouté une musique, vous pourrez voter entre chaque musique proposée par les utilisateurs. Les musiques récoltant le plus de votes seront celles 
+             qui seront jouées.
+        </p>
+    </div>
+</body>
+</html>
