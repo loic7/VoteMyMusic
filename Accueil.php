47021cce266d1eb0191c3e7e039d6126954a0446
Loïc M <loic.mahadzere.pro@gmail.com>
2024-04-18 14:23:18 +0200

    supression bug

--- Accueil.php
+++ Accueil.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<<<<<<< HEAD
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
@@ -38,26 +37,3 @@
             <li><a href="#">Contact</a></li>
         </ul>
     </footer>
-=======
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;500&display=swap" rel="stylesheet">
-    <title>TEST</title>
-</head>
-<body>
-    <div class="header">
-        <h1>Bienvenue</h1>
-    </div>
-
-    <div class="description">
-        <p>bienvenue sur le site VoteMymusic ici vous pourrez ajouter les musiques que vous voudriez entendre lors d'un concert karaoké ou activé en tous genre une
-             après avoir ajouté une musique, vous pourrez voter entre chaque musique proposée par les utilisateurs. Les musiques récoltant le plus de votes seront celles 
-             qui seront jouées.
-        </p>
-    </div>
->>>>>>> c764046345b67bf0ffdfa7734715ec79416dc48a
-</body>
-</html>
